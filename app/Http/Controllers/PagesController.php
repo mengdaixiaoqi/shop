2239e41c7ff5ed1fff8ea57189ea49306726cca6
mengdaixiaoqi <3069003066@qq.com>
2019-07-26 09:05:12 +0800

    邮箱验证中间间件

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    //首页
     public function root(){
 		return view('pages.root');
+	}
+
+	//
+	public function emailVerifyNotice(Request $request){
+		return view('pages.email_verify_notice');
 	}
 }
--- app/Http/Middleware/CheckIfVerified.php
+++ app/Http/Middleware/CheckIfVerified.php
@@ -15,6 +15,13 @@ class CheckIfVerified
      */
     public function handle($request, Closure $next)
     {
+        if(!$request->user()->email_verified){
+            //如果是AJAX请求，则通过 JSON 返回
+            if($request->expectsJson()){
+                return response()->json(['msg'=>'请先验证邮件',400]);
+            }
+            return redirect(route('email_verify_notice'));
+        }
         return $next($request);
     }
 }
