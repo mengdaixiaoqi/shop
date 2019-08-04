b5efb40645f637c92864b5b2d86d8584ae61c7b2
mengdaixiaoqi <1669003066@qq.com>
2019-08-04 23:11:28 +0800

    购物车页面

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductSku;
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
 use Illuminate\Http\Request;
@@ -31,5 +32,20 @@ class CartController extends Controller
         }
 
         return [];
+    }
+
+    public function index(Request $request)
+    {
+        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+
+        $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
+        return view('cart.index', ['cartItems' => $cartItems, 'addresses' => $addresses]);
+    }
+
+    public function remove(ProductSku $sku, Request $request)
+    {
+       $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+
+       return [];
     }
 }
