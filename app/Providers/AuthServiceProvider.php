112d12009df3b8ed76c70a59c7dbd9bea0c9c3a0
mengdaixiaoqi <1669003066@qq.com>
2019-08-07 23:27:07 +0800

    用户界面 - 订单详情页

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Order;
+use App\Policies\OrderPolicy;
 use App\Models\UserAddress;
 use App\Policies\UserAddressPolicy;
 use Illuminate\Support\Facades\Gate;
@@ -18,6 +20,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
         UserAddress::class => UserAddressPolicy::class,
+        Order::class => OrderPolicy::class,
     ];
 
     /**
