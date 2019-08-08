b9043930d14a53f02da1c368543712df6a684cb9
mengdaixiaoqi <1669003066@qq.com>
2019-08-08 22:49:24 +0800

    集成支付宝

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,10 @@ Route::group(['middleware'=>'auth'],function(){
     Route::get('products/{product}', 'ProductsController@show')->name('products.show');
     // 结束
 });
+// Route::get('alipay', function() {
+//     return app('alipay')->web([
+//         'out_trade_no' => time(),
+//         'total_amount' => '1',
+//         'subject' => 'test subject - 测试',
+//     ]);
+// });
