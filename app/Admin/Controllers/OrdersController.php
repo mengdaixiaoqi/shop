ccdc98b6238c5d9c7eb342076634d1a27372adf8
mengdaixiaoqi <3069003066@qq.com>
2019-08-11 22:24:44 +0800

    管理后台 - 订单详情

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -4,12 +4,13 @@ namespace App\Admin\Controllers;
 
 use App\Models\Order;
 use Encore\Admin\Controllers\AdminController;
+use App\Http\Controllers\Controller;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
 use Encore\Admin\Layout\Content;
 
-class OrdersController extends AdminController
+class OrdersController extends Controller
 {
     /**
      * Title for current resource.
@@ -24,6 +25,15 @@ class OrdersController extends AdminController
             ->header('订单列表')
             ->body($this->grid());
     }
+
+    //
+    public function show(Order $order, Content $content)
+    {
+        return $content
+            ->header('查看订单')
+            // body 方法可以接受 Laravel 的视图作为参数
+            ->body(view('admin.orders.show', ['order' => $order]));
+    }
     /**
      * Make a grid builder.
      *
@@ -35,17 +45,16 @@ class OrdersController extends AdminController
 
         // 只展示已支付的订单，并且默认按支付时间倒序排序
         $grid->model()->whereNotNull('paid_at')->orderBy('paid_at', 'desc');
-        //$grid->column('id', __('Id'));
-        $grid->column('no', '订单流水号');
+
+        $grid->no('订单流水号');
         // 展示关联关系的字段时，使用 column 方法
         $grid->column('user.name', '买家');
-        $grid->column('total_amount', '总金额')->sortable();
-        $grid->column('paid_at', '支付时间')->sortable();
-
-        $grid->column('ship_status', '物流')->display(function($value) {
+        $grid->total_amount('总金额')->sortable();
+        $grid->paid_at('支付时间')->sortable();
+        $grid->ship_status('物流')->display(function($value) {
             return Order::$shipStatusMap[$value];
         });
-        $grid->column('refund_status', '退款状态')->display(function($value) {
+        $grid->refund_status('退款状态')->display(function($value) {
             return Order::$refundStatusMap[$value];
         });
         // 禁用创建按钮，后台不需要创建订单
@@ -61,6 +70,7 @@ class OrdersController extends AdminController
                 $batch->disableDelete();
             });
         });
+
         return $grid;
     }
 
