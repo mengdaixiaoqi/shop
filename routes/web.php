472afb75191270261ce36557b4fc847391c9ca9a
mengdaixiaoqi <3069003066@qq.com>
2019-08-27 00:17:36 +0800

    管理后台 - 同意退款 - 微信支付退款

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,7 @@ Route::group(['middleware'=>'auth'],function(){
     // 结束
 });
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
+Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
 // Route::get('alipay', function() {
 //     return app('alipay')->web([
 //         'out_trade_no' => time(),
