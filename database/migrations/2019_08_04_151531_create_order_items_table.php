4528f24bd6f1e1b1473cb9434787213c76018132
mengdaixiaoqi <3069003066@qq.com>
2019-08-29 00:26:48 +0800

    管理后台 - 优惠券列表

--- database/migrations/2019_08_04_151531_create_order_items_table.php
+++ database/migrations/2019_08_04_151531_create_order_items_table.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOrderItemsTable extends Migration
+class
+CreateOrderItemsTable extends Migration
 {
     /**
      * Run the migrations.
